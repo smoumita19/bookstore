1a51b322daa766c3dac32b1968f6e779ac01bc65
Moumita Sen <52632685+smoumita19@users.noreply.github.com>
2022-02-21 15:39:16 +0530

    Add files via upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyFirstConsoleApp.Controllers
+{
+    //To make it a controller class you have to use 'Controller' class
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            return View();
+        }
+
+        public ViewResult AboutUs()
+        {
+            return View();
+        }
+    }
+}
