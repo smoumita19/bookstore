061b70c8d0f175d7d5b200a6a752328daa75a911
Moumita Sen <52632685+smoumita19@users.noreply.github.com>
2022-03-01 11:26:11 +0530

    Create BookController.cs

---
+++ bookstore/bookstore/Controllers/BookController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MyFirstConsoleApp.Repository;
+using MyFirstConsoleApp.Models;
+
+namespace MyFirstConsoleApp.Controllers
+{
+    public class BookController : Controller
+    {
+        private readonly BookRepository _bookRepository = null;
+        public BookController()
+        {
+            _bookRepository = new BookRepository();
+        }
+        public IActionResult Index()                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      
+        {
+            return View();
+        }
+
+        //returns all books in the application
+        public List<BookModel> GetAllBooks()
+        {
+            return _bookRepository.GetAllBooks();
+        }
+
+        public List<BookModel> SearchBooks(string bookName, string authorName)
+        {
+            return _bookRepository.SearchBook(bookName, authorName);
+
+        }
+
+        public BookModel GetBook(int id)
+        {
+            //return $"Book with id = {id}";
+            return _bookRepository.GetBookById(id);
+        }
+    }
+}
